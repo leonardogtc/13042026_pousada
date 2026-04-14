b680c5589ccabacdc82c76a9e824b1511aaad184
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-04-14 10:09:59 -0300

    Criação de uma estrutura mestre-detalhe usando Inlines. Alteração realizada no arquivo hospedagem/admin.py

--- hospedagem/admin.py
+++ hospedagem/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Quarto, Hospede, Estadia
 
+class AcompanhanteInline(admin.TabularInline):
+    model = Hospede
+    extra = 1 # Quantidade de linhas em branco que aparecerão por padrão
+    fk_name = 'responsavel' # Campo que relaciona o acompanhante ao responsável
+    verbose_name = 'Acompanhante'
+    verbose_name_plural = 'Acompanhantes'
+    
+@admin.register(Hospede)
+class HospedeAdmin(admin.ModelAdmin):
+    inlines = [AcompanhanteInline]
+    list_display = ('nome', 'cpf', 'email', 'telefone', 'eh_principal')
+    list_filter = ('eh_principal',)
+    search_fields = ('nome', 'cpf', 'email')
+
 admin.site.register(Quarto)
-admin.site.register(Hospede)
 admin.site.register(Estadia)
